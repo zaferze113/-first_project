009d2e3db8d1c11f1b256b4b44dffa6faaf1ec57
vidolexq <100509694+zaferze113@users.noreply.github.com>
2023-09-29 21:30:46 +0300

    Create sifre.py

---
+++ sifre.py
@@ -0,0 +1,10 @@
+import random 
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+prluzunluk = int(input("parola karakterinizi giriniz:"))
+
+sifre = ""
+                 
+for i in range(prluzunluk)
+    sifre = sifre+random.choice(karakterler)
+
+print(sifre)
